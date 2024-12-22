ffebee190cff6b33cfe0df97797ac44429c41d51
fulik0z <fulikozzz@gmail.com>
2024-12-22 17:27:25 +0700

    Added: function checkCollisionTank in tanks.cpp

--- Tanks/Tanks.cpp
+++ Tanks/Tanks.cpp
@@ -2,7 +2,67 @@
 #include "Map.h"
 
 #include "Player_Tank.h"
+
 using namespace sf;
+
+void checkCollisionTank(Tank& tank, Map& map) {
+	/*for (int y = (tank.getPosition().getY() + 5) / 54; y < (tank.getPosition().getY() + 49) / 54; y++) {
+		for (int x = (tank.getPosition().getX() + 5) / 54; x < (tank.getPosition().getX() + 49) / 54; x++) {
+			Wall rect = map.getCell(y, x);
+			if (rect.getType() == Empty || rect.getType() == Tree || rect.getType() == Ice) {
+				continue;
+			}
+			if (tank.getDirection() == UP) {
+				tank.setPosition(Position(tank.getPosition().getX(), y * 54 + 49));
+			}
+			if (tank.getDirection() == LEFT) {
+				tank.setPosition(Position(x * 54 + 49, tank.getPosition().getY()));
+			}
+			if (tank.getDirection() == DOWN) {
+				tank.setPosition(Position(tank.getPosition().getX(), y * 54 - 49));
+			}
+			if (tank.getDirection() == RIGHT) {
+				tank.setPosition(Position(x * 54 - 49, tank.getPosition().getY()));
+			}
+		}
+	}*/
+	// Размер клетки карты
+	const int cellSize = 54;
+
+	int tankTopLeftX = tank.getPosition().getX() / cellSize;
+	int tankTopLeftY = tank.getPosition().getY() / cellSize;
+	int tankBottomRightX = (tank.getPosition().getX() + cellSize - 1) / cellSize;
+	int tankBottomRightY = (tank.getPosition().getY() + cellSize - 1) / cellSize;
+
+	FloatRect tank_obj(tank.getPosition().getX(), tank.getPosition().getY(), 49, 49);
+	
+	for (int y = tankTopLeftY; y <= tankBottomRightY; ++y) {
+		for (int x = tankTopLeftX; x <= tankBottomRightX; ++x) {
+			Wall wall = map.getCell(y, x);
+			if (wall.getType() == Empty || wall.getType() == Tree || wall.getType() == Ice) {
+				continue; 
+			}
+			
+			FloatRect walls(x * cellSize, y * cellSize, cellSize, cellSize);
+
+			if (walls.intersects(tank_obj)) {
+				if (tank.getDirection() == UP) {
+					tank.setPosition(Position(tank.getPosition().getX(), walls.top + walls.height));
+				}
+				else if (tank.getDirection() == DOWN) {
+					tank.setPosition(Position(tank.getPosition().getX(), walls.top - tank_obj.height));
+				}
+				else if (tank.getDirection() == LEFT) {
+					tank.setPosition(Position(walls.left + walls.width, tank.getPosition().getY()));
+				}
+				else if (tank.getDirection() == RIGHT) {
+					tank.setPosition(Position(walls.left - tank_obj.width, tank.getPosition().getY()));
+				}
+			}
+		}
+	}
+}
+
 
 int main()
 {
@@ -36,6 +96,7 @@ int main()
 		map.draw(window);
 		// Включаем контроллер
 		ptank.control(time);
+		checkCollisionTank(ptank, map);
 		// Отрисовываем игрока
 		window.draw(ptank.getSprite());
 		window.display();
